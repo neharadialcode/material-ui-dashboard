4c95b4f49755feea1bb85866a03bb86ec4aaa70d
Neha Rai <neharadialcode@gmail.com>
2023-09-16 12:54:31 +0530

    dropdown issue not resolved

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,27 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import TreeView from "@mui/lab/TreeView";
 import TreeItem from "@mui/lab/TreeItem";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Box, Button } from "@mui/material";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import WbIncandescentIcon from "@mui/icons-material/WbIncandescent";
 
-const Dropdown = () => {
+const Dropdown = ({ id }) => {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+  const dropdownData = [
+    {
+      name: "first",
+      content: ["one", "Two", "Three", "Four", "Five", "Six"],
+      icon: <AutorenewIcon />,
+    },
+    {
+      name: "second",
+      content: ["second", "Two", "Three", "Four", "Five", "Six"],
+      icon: <AccountBalanceWalletIcon />,
+    },
+    {
+      name: "third",
+      content: ["third", "Two", "Three", "Four", "Five", "Six"],
+      icon: <AccountBalanceIcon />,
+    },
+    {
+      name: "third",
+      content: ["third", "Two", "Three", "Four", "Five", "Six"],
+      icon: <CardGiftcardIcon />,
+    },
+    {
+      name: "third",
+      content: ["third", "Two", "Three", "Four", "Five", "Six"],
+      icon: <WbIncandescentIcon />,
+    },
+  ];
   return (
-    <TreeView
-      aria-label="file system navigator"
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
-    >
-      <TreeItem nodeId="1" label="Applications">
-        <TreeItem nodeId="2" label="Calendar" />
-      </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="10" label="OSS" />
-        <TreeItem nodeId="6" label="MUI">
-          <TreeItem nodeId="8" label="index.js" />
-        </TreeItem>
-      </TreeItem>
-    </TreeView>
+    <>
+      {dropdownData.splice(id, 1).map((obj, i) => (
+        <>
+          <Box
+            sx={{
+              position: "relative",
+              zIndex: 1,
+            }}
+          >
+            <Button
+              sx={{
+                color: "color.white",
+                padding: 0,
+                position: "absolute",
+                left: "0px",
+                top: "0px",
+                minWidth: "auto",
+                zIndex: 0,
+              }}
+            >
+              {obj.icon}
+            </Button>
+            <TreeView
+              aria-label="file system navigator"
+              sx={{
+                overflowY: "auto",
+                overflowX: "hidden",
+                color: "color.white",
+                width: "100%",
+                zIndex: 1,
+                display: "block",
+              }}
+            >
+              <TreeItem
+                onClick={handleClick}
+                nodeId="1"
+                label={obj.name}
+                sx={{ position: "relative", zIndex: 2, padding: "0 0 0 0" }}
+              >
+                {obj.content.map((item, index) => (
+                  <TreeItem
+                    style={{ padding: 0 }}
+                    key={index}
+                    nodeId="2"
+                    label={item}
+                  />
+                ))}
+              </TreeItem>
+            </TreeView>
+            <Button
+              variant="text"
+              sx={{
+                color: "color.white",
+                padding: "0 0 0 0",
+                position: "absolute",
+                right: "10px",
+                top: "0px",
+                minWidth: "auto",
+              }}
+            >
+              {open ? <ChevronRightIcon /> : <ExpandMoreIcon />}
+            </Button>
+          </Box>
+        </>
+      ))}
+    </>
   );
 };
 
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,26 +1,50 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
+import Dropdown from "./Dropdown";
+import Image from "next/image";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
 
 const Sidebar = () => {
   return (
     <Box
       sx={{
         width: "250px",
-        bgcolor: "primary.secondary",
+        bgcolor: "color.black",
         color: "fontColor.abc",
         height: "100vh",
-        padding: "16px",
+        padding: "20px 0px",
       }}
     >
-      <Typography
-        variant="h4"
-        sx={{
-          fontSize: "text.xs",
-          color: "color.abc",
-        }}
-      >
-        Sidebar
-      </Typography>
+      <Image width={180} height={35} src="/assets/img/logo.png" />
+      <Box sx={{ overflow: "auto", height: "100%" }}>
+        <Typography
+          variant="h4"
+          sx={{ color: "color.white", padding: "20px 0" }}
+        >
+          Quick Access
+        </Typography>
+        <Box
+          className="sidebar_item"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <Button
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "flex-start",
+              padding: "0 0 0 30px",
+              color: "color.white",
+            }}
+          >
+            Dashboard
+          </Button>
+          <AutorenewIcon sx={{ position: "absolute", color: "color.white" }} />
+        </Box>
+        <Dropdown id={0} />
+        <Dropdown id={1} />
+        <Dropdown id={2} />
+        <Dropdown id={3} />
+      </Box>
     </Box>
   );
 };
